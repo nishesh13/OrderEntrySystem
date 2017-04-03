b9be8f17b21cfe3df9267232f2fb7931c2a88bc1
NPSingh <nishesh.pratap@gmail.com>
2017-04-03 19:51:31 +0530

    committed service class

---
+++ oes/src/main/java/org/nishesh/restful/oes/models/service/OrderEntryService.java
@@ -0,0 +1,30 @@
+package org.nishesh.restful.oes.models.service;
+
+import org.nishesh.restful.oes.models.Order;
+
+public class OrderEntryService {
+
+	public Order[] getOrders(){
+		return new Order[1];
+	}
+	
+	public Order getOrder(int id){
+		return new Order();
+	}
+	
+	public void submitOrder(Order order){
+		
+	}
+	
+	public void updateOrder(Order order){
+		
+	}
+	
+	public void deleteOrder(int id){
+		
+	}
+	
+	public void cancelOrder(int id){
+		
+	}
+}
